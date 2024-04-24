63b8f76c0520ee8c851ca176766bc4cebf559b0f
Ana Nunes <ananunes.dev@gmail.com>
2024-04-24 20:27:56 -0300

    teste básico de comparativos

---
+++ python-senac/teste.py
@@ -0,0 +1,25 @@
+nome = 'Ana'
+print(nome)
+
+idade = 19
+print(idade)
+
+numerofloat = 1265966.4456
+print(numerofloat)
+
+vouf = True
+print(vouf)
+
+recebervalor = 'recebo o valor digitado com um ='
+print(recebervalor)
+
+numerocomparativo = 2
+idade = 1+1
+
+if(numerocomparativo == idade):
+    print("Valores iguais")
+else: 
+    print("Valores são diferentes")
+
+while(numerocomparativo >= 2):
+    numerocomparativo = int(input('Digite sua idade: '))
