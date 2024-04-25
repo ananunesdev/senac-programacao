7978e50e29d10f504db362222bdf6bdcaf2bed80
Ana Nunes <ananunes.dev@gmail.com>
2024-04-24 21:43:53 -0300

    alterações de pasta

---
+++ python-senac/UC9/laco.for.py
@@ -0,0 +1,10 @@
+n1 = 0
+for opcao in range (0, 4 , 1): #início, limite, passo
+    #n1 = n1 + opcao                       #bloco de comandos
+    #print('Opção: ', opcao )
+    #print("Resultado: ", n1)
+
+    #opcao = opcao + 1
+    #print(opcao)
+    opcao = int(input('Qual sua idade? '))
+    print('Sua idade é ', opcao)
