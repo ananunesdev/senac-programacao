ead287fecc9924752aa05f90800e1e0d8c4cb9f0
Ana Nunes <ananunes.dev@gmail.com>
2024-05-02 19:15:00 -0300

    Update nomes.py

--- python-senac/UC9/nomes.py
+++ python-senac/UC9/nomes.py
@@ -1,7 +1,7 @@
 nomes = ['gustavo' , 'ana' , 'edneia' , 'euler' , 'guilherme']
  
-dado = input("Digite seu nome:").upper() #todas as letras ficarem maiúsculas
-teste = 0
+dado = input("Digite seu nome:").upper() #todas as letras ficarem maiúsculas e () é obrigatório
+teste = 0                               #title deixa só a primeira leta maiúscula e () é obrigatório
 for nome in nomes:
     tnome = nome.upper()
     if tnome == dado:
