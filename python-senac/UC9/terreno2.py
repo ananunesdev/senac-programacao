b4a85c5f1a0cc91bc5d43dc79f8d656fb3254bb2
Ana Nunes <ananunes.dev@gmail.com>
2024-05-07 21:17:33 -0300

    Update terreno2.py

--- python-senac/UC9/terreno2.py
+++ python-senac/UC9/terreno2.py
@@ -17,12 +17,48 @@ def metros_cubicos (area, altura):
 
 alt_laje = float(input('Digite a altura da laje: '))
 
-areia = 6 
-brita = 4
-cimento = 2
+alt_laje_m3 = metros_cubicos(area_terreno,alt_laje)
 
-total_material = [6, 4, 2]
+print(f'O valor da área em m³: {alt_laje_m3:.2f}m³')
 
-for material in total_material:
-    material_total = alt_laje * len(total_material)
-    print('Material total usado na obra: ', material_total)
+calc_areia = (alt_laje_m3 / 3) * 2
+calc_brita = (alt_laje_m3 / 2) * 2
+calc_cimento = (alt_laje_m3 / 1) * 2
+calc_peso_caminhao = (alt_laje_m3 * 2400)
+total_calc = calc_areia + calc_brita + calc_cimento
+
+print(f'===== MEDIDA PARA CARREGAMENTO DO CAMINHÃO =====')
+print(f'Qtd.: Areia: {calc_areia:.2f}\n')
+print(f'Qtd.: Brita: {calc_brita:.2f}\n')
+print(f'Qtd.: Cimento: {calc_cimento:.2f}\n')
+print('===== PESO DO CAMINHÃO EM M³ =====\n')
+print(f'Peso de Caminhão: {calc_peso_caminhao:.3f}\n')
+ 
+#Valor de custo de produção areia (189,03) / brita (175,00) / Cimento R$ 270
+#Calcular o valor de preço 530 por metros cubicos...
+ 
+#Base 12
+#6 areia       189,03 / 2 = 94,51
+#4 brita       175,00 / 3 = 58,33    =  197,84
+#2 cimento     270,00 / 6 = 45,00
+
+print( '====== CÁLCULO DE LUCRO =====' )
+Vareia = 189.03
+Vbrita = 175
+Vcimento = 270
+
+resultado_areia = ((Vareia / 12) * 6)
+resultado_brita = ((Vbrita / 12) * 4)
+resultado_cimento = ((Vcimento / 12) * 2)
+venda = float(530 * total_calc)
+
+valor_producao = resultado_areia + resultado_brita + resultado_cimento
+lucro = venda - valor_producao
+
+print()
+print(f'O valor da areia é {resultado_areia:.2f}\n'
+f'O valor da brita é {resultado_brita:.2f}\n'
+f'O valor da cimento é {resultado_cimento:.2f}\n'
+f'Preço de produção {valor_producao:.2f}\n'
+f'O valor da venda é {venda:.2f}\n'
+f'O lucro é de {lucro:.2f}')
