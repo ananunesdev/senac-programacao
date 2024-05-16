078f5a71461aa4fbfc533a3d5889899e58352fa7
Ana Nunes <ananunes.dev@gmail.com>
2024-05-15 21:43:55 -0300

    Update academia.prof.py

--- python-senac/UC9/academia.prof.py
+++ python-senac/UC9/academia.prof.py
@@ -1,3 +1,30 @@
+def exibir_ficha(paciente):
+    print('\nFicha de Paciente')
+    print('===================')
+    for chave, valor in paciente.items():
+        if chave == 'IMC' and valor is not None:
+            print(f'{chave}: {valor:.2f}')
+        else:
+            print(f'{chave}: {valor}')
+
+def avaliacao_imc (imc):
+    if imc is None:
+        return 'Altura e/ou peso inválidos. Não é possível calcular o IMC.'
+    elif imc < 18.5:
+        return 'Abaixo do peso. É recomendável uma dieta equilibrada e acompanhamento nutricional.'
+    elif 18.5 <= imc < 24.9:
+        return 'Peso normal. Continue com uma alimentação saudável e uma dieta balanceada.'
+    elif 25 <= imc < 29.9:
+        return 'Sobrepeso. Sugere-se um plano de exercício e alterações na alimentação.'
+    elif 30 <= imc < 34.9:
+        return 'Obesidade Grau I.'
+    elif 35 <= imc < 39.9:
+        return 'Obesidade Grau II.'
+    else:
+        return 'Obesidade Grau III.'
+    
+    #elif (imc >= 18.5) and (imc < 24.9):
+
 def calcular_imc(peso, altura):
     try:
         imc = peso / (altura ** 2)
@@ -13,13 +40,18 @@ def registrar_paciente():
 
     imc = calcular_imc(peso, altura)
 
+    avaliacao = avaliacao_imc(imc)
+
     paciente = {
         'Nome': nome,
         'Idade': idade,
         'Peso': peso,
         'Altura': altura,
-        'IMC': imc
+        'IMC': imc,
+        'Avaliacao': avaliacao
     }
+
+    return paciente
 
 def main():
     pacientes = []
@@ -33,11 +65,13 @@ def main():
         opcao = input('Escolha uma opção: ')
 
         if opcao == '1':
-            print('teste')
             paciente = registrar_paciente()
             pacientes.append(paciente)
         elif opcao == '2':
-            print('teste 2')
+            if pacientes:
+                exibir_ficha(pacientes[-1])
+            else:
+                print('Nenhum paciente registrado.')
         elif opcao == '3':
             print('Saindo...')
             break
